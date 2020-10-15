802173d91ed4c9df07d40f7b4039cc5755430ff3
路人甲 <717785320@qq.com>
2020-10-15 13:08:13 +0800

    update unipay.js.

--- unipay.js
+++ unipay.js
@@ -1,7 +1,9 @@
 
+//获取cookie重写配置: https://youhui.95516.com/newsign/public/app/index.html url script-request-header https://gitee.com/passerby-b/loon/blob/master/unipay.js
+//打开重写后进入云闪付签到页面,提示获得Cookie即可
+
 try {
     if (typeof $request != "undefined") {
-        console.log($request.url);
         if ($request.url.indexOf("youhui.95516.com/newsign/public/app/index.html") > -1) {
             var Cookie = $request.headers["Cookie"];
             $prefs.setValueForKey(Cookie, "UniCookie");
