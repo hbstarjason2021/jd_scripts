33a62f06663a20a788422c12976a1bad7d2122d3
路人甲 <717785320@qq.com>
2020-11-02 13:33:42 +0800

    update test.js.

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ var $tool = tool();
 try {
     if (!!$response.body) {
         var body = $response.body;
-        if (body.indexOf("download_addr") > -1 || body.indexOf("play_addr") > -1 || body.indexOf("video/tos/cn/tos-cn-ve") > -1) {
+        if (body.indexOf("isure6.stream.qqmusic.qq.com") > -1 ) {
             console.log("🍎🍎🍎🍎🍎🍎🍎🍎🍎🍎🍎");
             console.log("🍎" + $request.url + "🍎" + body);
             console.log("🍎🍎🍎🍎🍎🍎🍎🍎🍎🍎🍎");
