20dd1dcdac019971d7c07e771331e701e3bca452
路人甲 <717785320@qq.com>
2020-11-18 15:10:15 +0800

    update cydg.js.

--- cydg.js
+++ cydg.js
@@ -14,8 +14,7 @@ try {
         }
     }
     else {
-        console.log("🍎🍎🍎🍎");
-        var url = 'http://hl.cydgsx.com/m/s/Log/SaveWriteLog';
+        var url = 'http://119.23.212.32/m/s/Log/SaveWriteLog';
         var method = 'POST';
         var headers = {
             'Connection': 'keep-alive',
