17bfa71ecc304ce4a386f85b41e2fca0e7b0230c
路人甲 <717785320@qq.com>
2020-11-18 16:59:38 +0800

    update cydg.js.

--- cydg.js
+++ cydg.js
@@ -12,11 +12,11 @@ try {
                 $tool.notify("闯越顶岗实习!", "获得Cookie", Cookie);
             }
         }
+        $done({});
     }
     else {
         
         var url = 'https://hl.cydgsx.com/m/s/Log/SaveWriteLog';
-        var method = 'POST';
         var headers = {
             'Connection': 'keep-alive',
             'Content-Length': '292',
@@ -37,7 +37,6 @@ try {
 
         var myRequest = {
             url: url,
-            method: method,
             headers: headers,
             body: body
         };
@@ -45,8 +44,8 @@ try {
 
         $tool.post(myRequest, function (e, r, d) {
             console.log(e);
-            console.log(r);
-            console.log(d);
+            //console.log(r);
+            //console.log(d);
             if (d.indexOf("请重新登录") >-1 ) {
                 $tool.notify("闯越顶岗实习!", "签到失败!", "请重新获取Cookie!");
             }
@@ -66,4 +65,4 @@ console.log("🍎闯越顶岗实习签到脚本结束!");
 
 function tool() { var a = typeof $httpClient != "undefined"; var b = typeof $task != "undefined"; var c = { notify: function (i, f, h, g) { var e = {}; if (b) { if (!!g) { if (typeof g == "string") { e["open-url"] = g } if (!!g.url) { e["open-url"] = g.url } if (!!g.img) { e["media-url"] = g.img } $notify(i, f, h, e) } else { $notify(i, f, h) } } if (a) { if (!!g) { if (typeof g == "string") { e["openUrl"] = g } if (!!g.url) { e["openUrl"] = g.url } if (!!g.img) { e["mediaUrl"] = g.img } $notification.post(i, f, h, e) } else { $notification.post(i, f, h) } } }, get: function (e, f) { if (b) { if (typeof e == "string") { e = { url: e } } e["method"] = "GET"; $task.fetch(e).then(function (g) { f(null, d(g), g.body) }, function (g) { f(g.error, null, null) }) } if (a) { $httpClient.get(e, function (i, h, g) { f(i, d(h), g) }) } }, post: function (e, f) { if (b) { if (typeof e == "string") { e = { url: e } } e["method"] = "POST"; $task.fetch(e).then(function (g) { f(null, d(g), g.body) }, function (g) { f(g.error, null, null) }) } if (a) { $httpClient.post(e, function (i, h, g) { f(i, d(h), g) }) } }, unicode: function (e) { return unescape(e.replace(/\\u/gi, "%u")) }, decodeurl: function (e) { return decodeURIComponent(e) }, json2str: function (e) { return JSON.stringify(e) }, str2json: function (e) { return JSON.parse(e) }, setkeyval: function (f, e) { if (b) { $prefs.setValueForKey(f, e) } if (a) { $persistentStore.write(f, e) } }, getkeyval: function (e) { if (b) { return $prefs.valueForKey(e) } if (a) { return $persistentStore.read(e) } } }; function d(e) { if (e) { if (e.status) { e["statusCode"] = e.status } else { if (e.statusCode) { e["status"] = e.statusCode } } } return e } return c };
 
-//$done({});
+
