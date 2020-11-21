0d18d5a18b6e2c50d32380ba9d97151866ecee44
路人甲 <717785320@qq.com>
2020-11-21 19:19:02 +0800

    update dywm.js.

--- dywm.js
+++ dywm.js
@@ -4,4 +4,27 @@
 //MITM:*.amemv.com
 //需要到作者的作品列表里找到那个视频再下载
 
-eval(function(p,a,c,k,e,r){e=function(c){return c.toString(36)};if('0'.replace(0,e)==0){while(c--)r[e(c)]=k[c];k=[function(e){return r[e]||e}];e=function(){return'[2-9abdf-hj]'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('4.5("🍎抖音去水印脚本开始!");8 3=$response.3;d{f($request.url.indexOf("core-c-hl.amemv.com/g/v1/g/post")>-1&&!!3){8 2=h.parse(3);for(8 i=0;i<2.6.length;i++){8 7=2.6[i].9.7.a;2.6[i].9.download_addr.a=7;f(!!2.6[i].9.j.a){2.6[i].9.j.a=7}4.5("🍎播放地址:"+7)}$b({3:h.stringify(2)})}else{4.5("🍎🍎3为空");$b({})}}catch(e){4.5("🍎🍎🍎🍎d错误:"+e);$b({})}4.5("执行完成!!!!");',[],20,'||obj|body|console|log|aweme_list|play_addr|var|video|url_list|done||try||if|aweme|JSON||download_suffix_logo_addr'.split('|'),0,{}))
+console.log("🍎抖音去水印脚本开始!");
+var body = $response.body;
+try {
+    if ($request.url.indexOf("core-c-hl.amemv.com/aweme/v1/aweme/post") > -1 && !!body) {
+        var obj = JSON.parse(body);
+        for (var i = 0; i < obj.aweme_list.length; i++) {
+            var play_addr = obj.aweme_list[i].video.play_addr.url_list;
+            obj.aweme_list[i].video.download_addr.url_list = play_addr;
+            if(!!obj.aweme_list[i].video.download_suffix_logo_addr.url_list){
+                obj.aweme_list[i].video.download_suffix_logo_addr.url_list = play_addr;
+            }
+            console.log("🍎播放地址:" + play_addr);
+        }
+        $done({ body: JSON.stringify(obj) });
+    }
+    else {
+        console.log("🍎🍎body为空");
+        $done({});
+    }
+} catch (e) {
+    console.log("🍎🍎🍎🍎try错误:" + e);
+    $done({});
+}
+console.log("执行完成!!!!");
