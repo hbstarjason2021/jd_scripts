82683fe3d5a86eed4e7ab7b8e270d7724940fb18
路人甲 <717785320@qq.com>
2020-11-24 10:59:19 +0800

    add

---
+++ get_cookie.js
@@ -0,0 +1,166 @@
+﻿
+var $tool = tool();
+try {
+    console.log("🍎获取cookie脚本开始!");
+    
+    if (typeof $request != "undefined") {
+        if ($request.url.indexOf("youhui.95516.com/newsign/public/app/index.html") > -1) {
+            console.log("🍎云闪付获取cookie脚本开始!");
+            var img = "https://is5-ssl.mzstatic.com/image/thumb/Purple114/v4/53/bc/b5/53bcb52a-6c33-67cc-0c70-faf4ffbdb71e/AppIcon-0-0-1x_U007emarketing-0-0-0-6-0-0-85-220.png/230x0w.png";
+            var Cookie = $request.headers["Cookie"];
+            if (!!Cookie) {
+                console.log(Cookie);
+                $tool.setkeyval(Cookie, "UniCookie");
+                $tool.notify("云闪付获取Cookie!", "获得Cookie", Cookie, { img: img });
+            }
+        }
+
+        if ($request.url.indexOf("v1/user/homePageInfo") > -1) {
+            console.log("🍎威锋获取cookie脚本开始!");
+            var Cookie = $request.headers["X-Access-Token"];
+            if (!!Cookie) {
+                console.log(Cookie);
+                $tool.setkeyval(Cookie, "weifengCookie");
+                $tool.notify("威锋获取Cookie!", "获得Cookie", Cookie);
+            }
+        }
+
+        if ($request.url.indexOf("mwegame.qq.com/cfip/growth/ajax/getUserTaskRecord") > -1) {
+            console.log("🍎cf逆战获取token脚本开始!");
+            var url = $request.headers["Referer"];
+            var index = url.indexOf("token=");
+            if (index > -1) {
+                var token = url.substr(index + 6, 8);
+
+                console.log(token);
+                $tool.setkeyval(token, "cfnztoken");
+                $tool.notify("cf逆战获取token!", "获得token", token);
+            }
+        }
+    }
+
+} catch (e) {
+    console.log(e);
+    $tool.notify("🍎获取cookie脚本错误!", e, e);
+}
+
+
+//loon/quanx通用方法
+function tool() {
+    var isLoon = typeof $httpClient != "undefined";
+    var isQuanX = typeof $task != "undefined";
+
+    var obj = {
+        //通知
+        notify: function (title, subtitle, message, option) {
+            var option_obj = {};
+            if (isQuanX) {
+                if (!!option) {
+                    if (typeof option == "string") option_obj["open-url"] = option;
+                    if (!!option.url) option_obj["open-url"] = option.url;
+                    if (!!option.img) option_obj["media-url"] = option.img;
+                    $notify(title, subtitle, message, option_obj);
+                }
+                else {
+                    $notify(title, subtitle, message);
+                }
+            }
+            if (isLoon) {
+                if (!!option) {
+                    if (typeof option == "string") option_obj["openUrl"] = option;
+                    if (!!option.url) option_obj["openUrl"] = option.url;
+                    if (!!option.img) option_obj["mediaUrl"] = option.img;
+                    $notification.post(title, subtitle, message, option_obj);
+                }
+                else {
+                    $notification.post(title, subtitle, message);
+                }
+            }
+        },
+        //get请求
+        get: function (options, callback) {
+            if (isQuanX) {
+                if (typeof options == "string") options = { url: options }
+                options["method"] = "GET"
+                $task.fetch(options).then(function (response) {
+                    callback(null, adapterStatus(response), response.body);
+                }, function (reason) {
+                    callback(reason.error, null, null);
+                });
+            }
+            if (isLoon) {
+                $httpClient.get(options, function (error, response, body) {
+                    callback(error, adapterStatus(response), body);
+                })
+            }
+        },
+        //post请求
+        post: function (options, callback) {
+            if (isQuanX) {
+                if (typeof options == "string") options = { url: options }
+                options["method"] = "POST"
+                $task.fetch(options).then(function (response) {
+                    callback(null, adapterStatus(response), response.body);
+                }, function (reason) {
+                    callback(reason.error, null, null);
+                });
+            }
+            if (isLoon) {
+                $httpClient.post(options, function (error, response, body) {
+                    callback(error, adapterStatus(response), body);
+                })
+            }
+        },
+        //Unicode解码
+        unicode: function (str) {
+            return unescape(str.replace(/\\u/gi, '%u'));
+        },
+        //url解码
+        decodeurl: function (str) {
+            return decodeURIComponent(str);
+        },
+        //对象转字符串
+        json2str: function (obj) {
+            return JSON.stringify(obj);
+        },
+        //字符串转对象
+        str2json: function (str) {
+            return JSON.parse(str);
+        },
+        //数据持久化写入
+        setkeyval: function (value, key) {
+            if (isQuanX) {
+                $prefs.setValueForKey(value, key);
+            }
+            if (isLoon) {
+                $persistentStore.write(value, key);
+            }
+        },
+        //数据持久化读取
+        getkeyval: function (key) {
+            if (isQuanX) {
+                return $prefs.valueForKey(key);
+            }
+            if (isLoon) {
+                return $persistentStore.read(key);
+            }
+        }
+
+    };
+
+    function adapterStatus(response) {
+        if (response) {
+            if (response.status) {
+                response["statusCode"] = response.status;
+            } else if (response.statusCode) {
+                response["status"] = response.statusCode;
+            }
+        }
+        return response;
+    }
+
+    return obj;
+
+};
+
+$done({});
