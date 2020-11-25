25d778d5834adfb24ec4d1780f621fd3bfbfd091
路人甲 <717785320@qq.com>
2020-11-25 11:42:43 +0800

    update ppx.js.

--- ppx.js
+++ ppx.js
@@ -4,29 +4,28 @@
 console.log("🍎抖音去水印脚本开始!");
 var body = $response.body;
 try {
-    if ($request.url.indexOf("ib-hl.snssdk.com/bds/feed/stream") > -1 && !!body) {
+    if (!!body) {
         console.log("🍎🍎" + body + "🍎🍎🍎");
         var obj = JSON.parse(body);
         for (var i = 0; i < obj.data.data.length; i++) {
-            if (!obj.data.data[i].item.video) {
+            if (!!obj.data.data[i].item && !!obj.data.data[i].item.video) {
                 obj.data.data[i].item.video.video_download.url_list = obj.data.data[i].item.video.video_high.url_list;
                 console.log("🍎" + obj.data.data[i].item.video.video_download.url_list[0]);
             }
-            else if (!!obj.data.data[i].item.comments) {
+            else if (!!obj.data.data[i].item && !!obj.data.data[i].item.comments) {
                 for (var n = 0; n < obj.data.data[i].item.comments.length; n++) {
                     obj.data.data[i].item.comments[n].item.video.video_download.url_list = obj.data.data[i].item.comments[n].item.video.video_high.url_list;
                     console.log("🍎" + JSON.stringify(obj.data.data[i].item.comments[n].item.video.video_high.url_list[0]));
                 }
             }
-            else if (!!obj.data.data[i].block_info.cell_list) {
+            else if (!!obj.data.data[i].block_info && !!obj.data.data[i].block_info.cell_list) {
                 for (var n = 0; n < obj.data.data[i].block_info.cell_list.length; n++) {
-                    if (!!obj.data.data[i].block_info.cell_list[n].item) {
+                    if (!!obj.data.data[i].block_info.cell_list[n].item && !!obj.data.data[i].block_info.cell_list[n].item.video) {
                         obj.data.data[i].block_info.cell_list[n].item.video.video_download.url_list = obj.data.data[i].block_info.cell_list[n].item.video.video_high.url_list;
                         console.log("🍎" + JSON.stringify(obj.data.data[i].block_info.cell_list[n].item.video.video_high.url_list[0]));
-                    }  
+                    }
                 }
             }
-            
         }
         $done({ body: JSON.stringify(obj) });
     }
