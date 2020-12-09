b2f07317507690b213bd3e331e4a1e40a57e3897
路人甲 <717785320@qq.com>
2020-12-09 09:08:05 +0800

    删除文件 luckbox_req.js

--- luckbox_req.js
+++
@@ -1,6 +0,0 @@
-console.log("🍎req脚本开始!");
-var headers = $request.headers;
-headers["response-format"] = "json";
-console.log("🍎response-format:" + headers["response-format"]);
-$done({ headers: headers });
-console.log("🍎执行完成!!!!");
