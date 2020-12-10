73d821279caefc2ab5f671624efb3d255aa5db81
路人甲 <717785320@qq.com>
2020-12-10 09:51:06 +0800

    update unipay.js.

--- unipay.js
+++ unipay.js
@@ -16,6 +16,7 @@ try {
             if (!!Cookie) {
                 $tool.setkeyval(Cookie, "UniCookie");
                 $tool.notify("云闪付签到!", "获得Cookie", Cookie, { img: img });
+                $done({});
             }
         }
     }
@@ -53,13 +54,16 @@ try {
                         }
                     }
                     $tool.notify("云闪付签到成功!", "首次签到时间:" + obj.startedAt, "已签到:" + days + "天!", { img: img });
+                    $done();
                 }
                 else {
                     $tool.notify("云闪付签到失败!", d, d, { img: img });
+                    $done();
                 }
             }
             else {
                 $tool.notify("云闪付签到失败!", d, d, { img: img });
+                $done();
             }
         })
         
@@ -68,9 +72,10 @@ try {
 } catch (e) {
     console.log(e);
     $tool.notify("云闪付签到错误!", e, e, { img: img });
+    $done();
 }
 
 
 function tool(){var isLoon=typeof $httpClient!="undefined";var isQuanX=typeof $task!="undefined";var obj={notify:function(title,subtitle,message,option){var option_obj={};if(isQuanX){if(!!option){if(typeof option=="string"){option_obj["open-url"]=option}if(!!option.url){option_obj["open-url"]=option.url}if(!!option.img){option_obj["media-url"]=option.img}$notify(title,subtitle,message,option_obj)}else{$notify(title,subtitle,message)}}if(isLoon){if(!!option){if(typeof option=="string"){option_obj["openUrl"]=option}if(!!option.url){option_obj["openUrl"]=option.url}if(!!option.img){option_obj["mediaUrl"]=option.img}$notification.post(title,subtitle,message,option_obj)}else{$notification.post(title,subtitle,message)}}},get:function(options,callback){if(isQuanX){if(typeof options=="string"){options={url:options}}options["method"]="GET";$task.fetch(options).then(function(response){callback(null,adapterStatus(response),response.body)},function(reason){callback(reason.error,null,null)})}if(isLoon){$httpClient.get(options,function(error,response,body){callback(error,adapterStatus(response),body)})}},post:function(options,callback){if(isQuanX){if(typeof options=="string"){options={url:options}}options["method"]="POST";$task.fetch(options).then(function(response){callback(null,adapterStatus(response),response.body)},function(reason){callback(reason.error,null,null)})}if(isLoon){$httpClient.post(options,function(error,response,body){callback(error,adapterStatus(response),body)})}},unicode:function(str){return unescape(str.replace(/\\u/gi,"%u"))},decodeurl:function(str){return decodeURIComponent(str)},json2str:function(obj){return JSON.stringify(obj)},str2json:function(str){return JSON.parse(str)},setkeyval:function(value,key){if(isQuanX){$prefs.setValueForKey(value,key)}if(isLoon){$persistentStore.write(value,key)}},getkeyval:function(key){if(isQuanX){return $prefs.valueForKey(key)}if(isLoon){return $persistentStore.read(key)}}};function adapterStatus(response){if(response){if(response.status){response["statusCode"]=response.status}else{if(response.statusCode){response["status"]=response.statusCode}}}return response}return obj};
 
-$done({});
+
