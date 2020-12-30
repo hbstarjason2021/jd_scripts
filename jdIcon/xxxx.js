426ae7904d825e4af29aed5a56b7656a5c586f13
路人甲 <717785320@qq.com>
2020-12-30 13:51:24 +0800

    update jdIcon/xxxx.js.

--- jdIcon/xxxx.js
+++ jdIcon/xxxx.js
@@ -2,10 +2,14 @@
 try {
     //language=zh&extensions=all&key=ecaf271a6890b98e35395cac340c98d1&output=json&location=114.301415%2C30.544527&radius=1000&scode=cb901f3d7d1b048c7aabfec344567a92&ts=1609306382118
     
-    $done({ body: 'language=zh&extensions=all&key=334650026fcf5b7ceb675e4c2f7eb7d1&output=json&location=114.3198823%2C30.471880&radius=1000&ts=1609306382118' });
+    var longitude = "114.3196" + parseInt(Math.random() * (99 - 10 + 1) + 10, 10) + "26257" + parseInt(Math.random() * (99 - 10 + 1) + 10, 10);//114.3196302625787
+    var latitude = "30.4720" + parseInt(Math.random() * (99 - 10 + 1) + 10, 10) + "292101" + parseInt(Math.random() * (99 - 10 + 1) + 10, 10);//30.47208829210171
+    var body = 'language=zh&extensions=all&key=334650026fcf5b7ceb675e4c2f7eb7d1&output=json&location=' + longitude + '%2C' + latitude + '&radius=1000&ts=' + Math.round(new Date()) + '';
+    console.log("🍎" + body);
+    $done({ body: body });
+    
     
 } catch (e) {
-    $done({ body: $response.body });
+    $done({ body: $request.body });
 }
-
 
