178db826ea51ba1f52130bb03bb4e5bb804ec894
路人甲 <717785320@qq.com>
2021-01-09 12:56:03 +0800

    add dyjs.js.

---
+++ dyjs.js
@@ -0,0 +1,148 @@
+
+console.log("🍎抖音极速脚本开始!");
+var $tool = tool();
+
+try {
+    if (!!$request) {
+        var headers = $request.headers;
+        var headlist = $tool.getkeyval("dyheadlist");
+        if (!!headers) {
+            if (!!headlist) {
+                var list = JSON.parse(headlist);
+                list.push(headers);
+            }
+            else {
+                var list = [];
+                list.push(headers);
+            }
+            $tool.notify("获取headers成功", "个数:" + list.length, "");
+            $tool.setkeyval(JSON.stringify(list), "dyheadlist");
+            console.log("🍎" + JSON.stringify(list));
+        }
+    }
+    else {
+
+    }
+} catch (e) {
+
+}
+
+$done({});
+
+//loon/quanx通用方法
+function tool() {
+    var isLoon = typeof $httpClient != "undefined";
+    var isQuanX = typeof $task != "undefined";
+
+    var obj = {
+        //通知
+        notify: function (title, subtitle, message, option) {
+            var option_obj = {};
+            if (isQuanX) {
+                if (!!option) {
+                    if (typeof option == "string") option_obj["open-url"] = option;
+                    if (!!option.url) option_obj["open-url"] = option.url;
+                    if (!!option.img) option_obj["media-url"] = option.img;
+                    $notify(title, subtitle, message, option_obj);
+                }
+                else {
+                    $notify(title, subtitle, message);
+                }
+            }
+            if (isLoon) {
+                if (!!option) {
+                    if (typeof option == "string") option_obj["openUrl"] = option;
+                    if (!!option.url) option_obj["openUrl"] = option.url;
+                    if (!!option.img) option_obj["mediaUrl"] = option.img;
+                    $notification.post(title, subtitle, message, option_obj);
+                }
+                else {
+                    $notification.post(title, subtitle, message);
+                }
+            }
+        },
+        //get请求
+        get: function (options, callback) {
+            if (isQuanX) {
+                if (typeof options == "string") options = { url: options }
+                options["method"] = "GET"
+                $task.fetch(options).then(function (response) {
+                    callback(null, adapterStatus(response), response.body);
+                }, function (reason) {
+                    callback(reason.error, null, null);
+                });
+            }
+            if (isLoon) {
+                $httpClient.get(options, function (error, response, body) {
+                    callback(error, adapterStatus(response), body);
+                })
+            }
+        },
+        //post请求
+        post: function (options, callback) {
+            if (isQuanX) {
+                if (typeof options == "string") options = { url: options }
+                options["method"] = "POST"
+                $task.fetch(options).then(function (response) {
+                    callback(null, adapterStatus(response), response.body);
+                }, function (reason) {
+                    callback(reason.error, null, null);
+                });
+            }
+            if (isLoon) {
+                $httpClient.post(options, function (error, response, body) {
+                    callback(error, adapterStatus(response), body);
+                })
+            }
+        },
+        //Unicode解码
+        unicode: function (str) {
+            return unescape(str.replace(/\\u/gi, '%u'));
+        },
+        //url解码
+        decodeurl: function (str) {
+            return decodeURIComponent(str);
+        },
+        //对象转字符串
+        json2str: function (obj) {
+            return JSON.stringify(obj);
+        },
+        //字符串转对象
+        str2json: function (str) {
+            return JSON.parse(str);
+        },
+        //数据持久化写入
+        setkeyval: function (value, key) {
+            if (isQuanX) {
+                $prefs.setValueForKey(value, key);
+            }
+            if (isLoon) {
+                $persistentStore.write(value, key);
+            }
+        },
+        //数据持久化读取
+        getkeyval: function (key) {
+            if (isQuanX) {
+                return $prefs.valueForKey(key);
+            }
+            if (isLoon) {
+                return $persistentStore.read(key);
+            }
+        }
+
+    };
+
+    function adapterStatus(response) {
+        if (response) {
+            if (response.status) {
+                response["statusCode"] = response.status;
+            } else if (response.statusCode) {
+                response["status"] = response.statusCode;
+            }
+        }
+        return response;
+    }
+
+    return obj;
+
+};
