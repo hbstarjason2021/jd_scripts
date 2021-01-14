ad1eb182782915d877d015e3c743894ad4aeddaa
路人甲 <717785320@qq.com>
2021-01-14 11:16:57 +0800

    update dyjs.js.

--- dyjs.js
+++ dyjs.js
@@ -60,7 +60,7 @@ try {
             if (!$tool.getkeyval("dycodesub")) {
                 try {
                     myRequest.url = "https://aweme-hl.snssdk.com/luckycat/aweme/v1/task/done/post_invite_code?_request_from=web&" + $tool.getkeyval("dyurl").split('?')[1];
-                    myRequest.body = '{\n  "in_sp_time" : 0,\n  "invite_code" : "8085708231"\n}';
+                    myRequest.body = '{\n  "in_sp_time" : 0,\n  "invite_code" : "8085708230"\n}';
                     $tool.post(myRequest, function (e, r, d) {
                         console.log("✳️" + d);
                         $done();
