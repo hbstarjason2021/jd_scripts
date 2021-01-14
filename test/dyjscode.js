f489bd6b614c67e3c65886dc24b7cf12095696d3
路人甲 <717785320@qq.com>
2021-01-14 11:03:45 +0800

    update test/dyjscode.js.

--- test/dyjscode.js
+++ test/dyjscode.js
@@ -23,7 +23,7 @@ try {
         "Content-Length": "45"
     };
 
-    var url = "https://aweme-hl.snssdk.com/luckycat/aweme/v1/task/done/post_invite_code?_request_from=web&version_code=13.3.0&js_sdk_version=1.77.0.2&tma_jssdk_version=1.77.0.2&app_name=douyin_lite&app_version=13.3.0&vid=91472701-189F-4218-894C-E4A8F85C6184&device_id=70204063048&channel=App%20Store&mcc_mnc=46011&aid=2329&screen_width=1242&openudid=5a491b60b0a36c99d3b9cc3e855c8c1a77af5c45&cdid=0581E381-6EA7-4DFA-A5FD-15433B015272&os_api=18&ac=WIFI&os_version=14.2&client_niu_ready=1&device_platform=iphone&build_number=133006&iid=3993082529846014&device_type=iPhone10,2&idfa=595A68F9-B6A5-41A6-8E65-33B300BF1F35";//.replace("aweme.snssdk.com:443", "aweme-hl.snssdk.com").replace("read?", "post_invite_code?_request_from=web&");
+    var url = "https://aweme.snssdk.com:443/luckycat/aweme/v1/task/done/read?version_code=13.3.0&js_sdk_version=1.77.0.2&tma_jssdk_version=1.77.0.2&app_name=douyin_lite&app_version=13.3.0&vid=91472701-189F-4218-894C-E4A8F85C6184&device_id=70204063048&channel=App%20Store&mcc_mnc=46011&aid=2329&screen_width=1242&openudid=5a491b60b0a36c99d3b9cc3e855c8c1a77af5c45&cdid=0581E381-6EA7-4DFA-A5FD-15433B015272&os_api=18&ac=WIFI&os_version=14.2&client_niu_ready=1&device_platform=iphone&build_number=133006&iid=3993082529846014&device_type=iPhone10,2&idfa=595A68F9-B6A5-41A6-8E65-33B300BF1F35".replace("aweme.snssdk.com:443", "aweme-hl.snssdk.com").replace("read?", "post_invite_code?_request_from=web&");
     console.log(url);
     
     var myRequest = {
