c023c046a98b109ecd95a5852a5fee41d25ea9be
路人甲 <717785320@qq.com>
2021-01-15 16:19:49 +0800

    update ksjs.js.

--- ksjs.js
+++ ksjs.js
@@ -4,8 +4,11 @@ var $tool = tool();
 try {
     if (typeof $request != "undefined") {
         console.log("🍎快手极速获取url脚本开始!");
+
         var url = $request.url;
+        var headers = $request.headers;
         var urllist = $tool.getkeyval("ksurllist");
+        $tool.setkeyval(JSON.stringify(headers), "ksheaders");
         if (!!url) {
             if (!!urllist) {
                 var list = JSON.parse(urllist);
@@ -47,14 +50,17 @@ try {
                 console.log("☢️第0个url!");
             }
 
-            var headers = {
-                'Accept-Encoding': 'gzip, deflate',
-                'X-REQUESTID': Math.round(new Date())+'85627',
-                'Connection': 'keep-alive',
-                'Accept': 'application/json',
-                'User-Agent': 'kwai-ios',
-                'Accept-Language': 'zh-Hans-CN;q=1, en-CN;q=0.9'
-            };
+            //var headers = {
+            //    'Accept-Encoding': 'gzip, deflate',
+            //    'X-REQUESTID': Math.round(new Date()) + '85627',
+            //    'Connection': 'keep-alive',
+            //    'Accept': 'application/json',
+            //    'User-Agent': 'kwai-ios',
+            //    'Accept-Language': 'zh-Hans-CN;q=1, en-CN;q=0.9'
+            //};
+            var headers = $tool.getkeyval("ksheaders");
+            headers = JSON.parse(headers);
+            headers['X-REQUESTID'] = Math.round(new Date()) + '85627';
             var myRequest = {
                 url: $tool.getkeyval("ksthisurl"),
                 headers: headers
@@ -81,4 +87,4 @@ try {
     $done();
 }
 
-function tool(){var isLoon=typeof $httpClient!="undefined";var isQuanX=typeof $task!="undefined";var obj={notify:function(title,subtitle,message,option){var option_obj={};if(isQuanX){if(!!option){if(typeof option=="string"){option_obj["open-url"]=option}if(!!option.url){option_obj["open-url"]=option.url}if(!!option.img){option_obj["media-url"]=option.img}$notify(title,subtitle,message,option_obj)}else{$notify(title,subtitle,message)}}if(isLoon){if(!!option){if(typeof option=="string"){option_obj["openUrl"]=option}if(!!option.url){option_obj["openUrl"]=option.url}if(!!option.img){option_obj["mediaUrl"]=option.img}$notification.post(title,subtitle,message,option_obj)}else{$notification.post(title,subtitle,message)}}},get:function(options,callback){if(isQuanX){if(typeof options=="string"){options={url:options}}options["method"]="GET";$task.fetch(options).then(function(response){callback(null,adapterStatus(response),response.body)},function(reason){callback(reason.error,null,null)})}if(isLoon){$httpClient.get(options,function(error,response,body){callback(error,adapterStatus(response),body)})}},post:function(options,callback){if(isQuanX){if(typeof options=="string"){options={url:options}}options["method"]="POST";$task.fetch(options).then(function(response){callback(null,adapterStatus(response),response.body)},function(reason){callback(reason.error,null,null)})}if(isLoon){$httpClient.post(options,function(error,response,body){callback(error,adapterStatus(response),body)})}},unicode:function(str){return unescape(str.replace(/\\u/gi,"%u"))},decodeurl:function(str){return decodeURIComponent(str)},json2str:function(obj){return JSON.stringify(obj)},str2json:function(str){return JSON.parse(str)},setkeyval:function(value,key){if(isQuanX){$prefs.setValueForKey(value,key)}if(isLoon){$persistentStore.write(value,key)}},getkeyval:function(key){if(isQuanX){return $prefs.valueForKey(key)}if(isLoon){return $persistentStore.read(key)}},wait:function(time){return new Promise(function(resolve){setTimeout(function(){console.log("🕒等待"+time+"毫秒");resolve(true)},time)})}};function adapterStatus(response){if(response){if(response.status){response["statusCode"]=response.status}else{if(response.statusCode){response["status"]=response.statusCode}}}return response}return obj};
+function tool() { var isLoon = typeof $httpClient != "undefined"; var isQuanX = typeof $task != "undefined"; var obj = { notify: function (title, subtitle, message, option) { var option_obj = {}; if (isQuanX) { if (!!option) { if (typeof option == "string") { option_obj["open-url"] = option } if (!!option.url) { option_obj["open-url"] = option.url } if (!!option.img) { option_obj["media-url"] = option.img } $notify(title, subtitle, message, option_obj) } else { $notify(title, subtitle, message) } } if (isLoon) { if (!!option) { if (typeof option == "string") { option_obj["openUrl"] = option } if (!!option.url) { option_obj["openUrl"] = option.url } if (!!option.img) { option_obj["mediaUrl"] = option.img } $notification.post(title, subtitle, message, option_obj) } else { $notification.post(title, subtitle, message) } } }, get: function (options, callback) { if (isQuanX) { if (typeof options == "string") { options = { url: options } } options["method"] = "GET"; $task.fetch(options).then(function (response) { callback(null, adapterStatus(response), response.body) }, function (reason) { callback(reason.error, null, null) }) } if (isLoon) { $httpClient.get(options, function (error, response, body) { callback(error, adapterStatus(response), body) }) } }, post: function (options, callback) { if (isQuanX) { if (typeof options == "string") { options = { url: options } } options["method"] = "POST"; $task.fetch(options).then(function (response) { callback(null, adapterStatus(response), response.body) }, function (reason) { callback(reason.error, null, null) }) } if (isLoon) { $httpClient.post(options, function (error, response, body) { callback(error, adapterStatus(response), body) }) } }, unicode: function (str) { return unescape(str.replace(/\\u/gi, "%u")) }, decodeurl: function (str) { return decodeURIComponent(str) }, json2str: function (obj) { return JSON.stringify(obj) }, str2json: function (str) { return JSON.parse(str) }, setkeyval: function (value, key) { if (isQuanX) { $prefs.setValueForKey(value, key) } if (isLoon) { $persistentStore.write(value, key) } }, getkeyval: function (key) { if (isQuanX) { return $prefs.valueForKey(key) } if (isLoon) { return $persistentStore.read(key) } }, wait: function (time) { return new Promise(function (resolve) { setTimeout(function () { console.log("🕒等待" + time + "毫秒"); resolve(true) }, time) }) } }; function adapterStatus(response) { if (response) { if (response.status) { response["statusCode"] = response.status } else { if (response.statusCode) { response["status"] = response.statusCode } } } return response } return obj };
