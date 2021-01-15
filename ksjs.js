f3016593a5f32165b5f732c8adb4dc3a5b49eb54
路人甲 <717785320@qq.com>
2021-01-15 20:37:18 +0800

    update ksjs.js.

--- ksjs.js
+++ ksjs.js
@@ -71,12 +71,10 @@ try {
                         myRequest.url = "https://nebula.kuaishou.com/rest/n/nebula/activity/earn/overview?addressBookAccessStatus=false";
                         myRequest.headers['Host'] = "nebula.kuaishou.com";
                         myRequest.headers['Cookie'] = $tool.getkeyval("kscookie");
-                        //myRequest.headers['Cookie'] = 'appver=9.0.10.460; c=a; client_key=63b2bdd7; countryCode=cn; egid=DFP6D65764A700DADA04BDBE97A2DC7717F36442AB9D425361438F7E880DBFEA; gid=DFP6D65764A700DADA04BDBE97A2DC7717F36442AB9D425361438F7E880DBFEA; kpf=IPHONE; kpn=NEBULA; kuaishou.api_st=Cg9rdWFpc2hvdS5hcGkuc3QSsAFDtgIRZCHQCP3GR0tIeus5O2ZnSMf0XLP1B-HN_yAbphzKrRMM7vUYvhkNd1GaFWgMAeeTORq36PoTr09fWGjRX4ncD2wcUsl3UE2IAClO4yqkyMzjN_Q8rqcjMHFdacyLuc4qnJUmJ1pOxxhWtQXwRVtvnDnCyTKMO7FmODueHXqSJjsoZr4kb2YdmfnK4pcH4vcMRnk8J7qmwGyniA_SGelqXk6Z88-CTl67tGE1LBoSAbBL62FCSBK90AGy_bo9cyIbIiAZAe2WRP8_wnaPMBtO--dKBglsuxRa-h1rLo1LJv7yfCgFMAE; kuaishou.h5_st=Cg5rdWFpc2hvdS5oNS5zdBKgAQWycgnOzuqeSQDvW4JnjASAzj9DEyYewqbT1qwdioX8c8Acn9mzwTsPZzz5YF_s1OgIiUxao5q8_FLBDdnc2r9CeokmBarsriJPHRPJsRmiWMDym8wQiXE2EjYcmrU9eQYx96v-LMPd6hf6pCtQofn2jjT2BHLwKmkVVbq70k_xBAsQYQYlutdB41DhLNTeFmw5qiw9gRXqL2wvIdYgVQUaEvphX81WaAWh_Ys27fjFOfHVkCIgKg9Px2SnrOLin423hVy3RdQu9kl0sOZBDTGHbGZH1Y4oBTAB; kuaishou.sixin.login_st=ChdrdWFpc2hvdS5zaXhpbi5sb2dpbi5zdBKgAdLvV-7ET_Fr_XtLRhNPmpBjSZBN07wJ5ca6er54naWDdiAzcQIAH1_AVRWNYQQDyw35d6U5r2JhppEZqMeiRFB9gWk4fB1iRxucE-yo5LkeRoHGy2-_w4CEO4qGje5qTV29QyDEFWVmjICOWxfQASsePZEvee7TP62wRStL6gxBoKys6KCmK2hWaccGNKWt2KzCeUPAKHfPQBAALmIBp48aEk8ZgyzV6kDTiIME3Bz8GqJ7ViIgZigAg7nJMzEqXwJF8is3cXje3UPwD7elnj5JXwDl6wgoBTAB; language=zh-Hans-CN%3Bq%3D1%2C%2520en-CN%3Bq%3D0.9; lat=0.000000; lon=0.000000; mod=iPhone11%2C6; net=WIFI; sys=iOS_14.1; token=Cg9rdWFpc2hvdS5hcGkuc3QSsAFDtgIRZCHQCP3GR0tIeus5O2ZnSMf0XLP1B-HN_yAbphzKrRMM7vUYvhkNd1GaFWgMAeeTORq36PoTr09fWGjRX4ncD2wcUsl3UE2IAClO4yqkyMzjN_Q8rqcjMHFdacyLuc4qnJUmJ1pOxxhWtQXwRVtvnDnCyTKMO7FmODueHXqSJjsoZr4kb2YdmfnK4pcH4vcMRnk8J7qmwGyniA_SGelqXk6Z88-CTl67tGE1LBoSAbBL62FCSBK90AGy_bo9cyIbIiAZAe2WRP8_wnaPMBtO--dKBglsuxRa-h1rLo1LJv7yfCgFMAE; userId=1693210678; ver=9.0; apptype=2; browseType=3; country_code=cn; cs=false; darkMode=false; foreign=0; ftt=K-T-T; global_id=DFP6D65764A700DADA04BDBE97A2DC7717F36442AB9D425361438F7E880DBFEA; isp=CUCC; os=14.1; power_mode=0; session_id=6A8CDF21-F9B2-4478-97C7-2496541AA286; sh=2688; sid=6A8CDF21-F9B2-4478-97C7-2496541AA286; sw=1242; ud=1693210678; weblogger_switch=; did=00E22B7F-16F2-4B5A-8B20-A640366C2C94'
-
                         $tool.get(myRequest, function (e2, r2, d2) {
-                            //d = JSON.parse(d);
-                            //console.log("♥️总金币:" + d.data.totalCoin);
-                            console.log("♥️总金币:" + d2);
+                            d = JSON.parse(d);
+                            console.log("♥️总现金:" + d.data.totalCash + "♥️总金币:" + d.data.totalCoin);
+                            //console.log("♥️总金币:" + d2);
                             $done();
                         })
                         //$done();
