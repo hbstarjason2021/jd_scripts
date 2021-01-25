c96da024e2147b6bb2f32585d2773d267ca87b07
路人甲 <717785320@qq.com>
2021-01-25 16:50:45 +0800

    update test/HTTP_backend.js.

--- test/HTTP_backend.js
+++ test/HTTP_backend.js
@@ -1,5 +1,5 @@
 // JavaScript source code
 
-var html = '<!DOCTYPE html><html lang="en"xmlns="http://www.w3.org/1999/xhtml"><head><meta charset="utf-8"/><title></title><script src="https://ajax.aspnetcdn.com/ajax/jquery/jquery-1.10.2.min.js"></script></head><body>测试:<input id="txtkey"type="text"/><input id="btn"type="button"value="测试"/></body></html><script>$(function(){$("#btn").click(function(){var keyval=$("#txtkey").val();alert($prefs.valueForKey(keyval))})})</script>';
-
+var html = '<!DOCTYPE html><html lang="en"xmlns="http://www.w3.org/1999/xhtml"><head><meta charset="utf-8"/><title></title><script src="https://ajax.aspnetcdn.com/ajax/jquery/jquery-1.10.2.min.js"></script></head><body>测试:<input id="txtkey"type="text"/><input id="btn"type="button"value="测试"/></body></html><script>$(function(){$("#btn").click(function(){var keyval=$("#txtkey").val()})})</script>';
+var a = $prefs.valueForKey("dyheadlist")
 $done(html);
