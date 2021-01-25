f44ca4a8485761520ec297d2a516086fe730d94e
路人甲 <717785320@qq.com>
2021-01-25 16:51:23 +0800

    update test/HTTP_backend.js.

--- test/HTTP_backend.js
+++ test/HTTP_backend.js
@@ -2,4 +2,4 @@
 
 var html = '<!DOCTYPE html><html lang="en"xmlns="http://www.w3.org/1999/xhtml"><head><meta charset="utf-8"/><title></title><script src="https://ajax.aspnetcdn.com/ajax/jquery/jquery-1.10.2.min.js"></script></head><body>测试:<input id="txtkey"type="text"/><input id="btn"type="button"value="测试"/></body></html><script>$(function(){$("#btn").click(function(){var keyval=$("#txtkey").val()})})</script>';
 var a = $prefs.valueForKey("dyheadlist")
-$done(html);
+$done(a);
