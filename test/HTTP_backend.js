41a3ecdf1274e1e8a7c9793bfea98d0f29aad7ff
路人甲 <717785320@qq.com>
2021-01-25 16:35:12 +0800

    update test/HTTP_backend.js.

--- test/HTTP_backend.js
+++ test/HTTP_backend.js
@@ -1,6 +1,6 @@
 // JavaScript source code
 
 //var html = '测试:<input type="text"/><input type="button" id="btn" value="测试" />$("#btn").click(function () {console.log(1111111);})';
-var html = '<!DOCTYPE html><html lang="en" xmlns="http://www.w3.org/1999/xhtml"><head><meta charset="utf-8" /><title></title></head><body>测试:<input type="text"/><input id="btn" type="button" value="测试" /></body></html>';
+var html = '<!DOCTYPE html><html lang="en"xmlns="http://www.w3.org/1999/xhtml"><head><meta charset="utf-8"/><title></title></head><body>测试:<input type="text"/><input id="btn"type="button"value="测试"/></body></html><script>$(function(){$("#btn").click(function(){alert(1111)})})</script>';
 
 $done(html);
