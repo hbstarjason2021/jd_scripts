ef354d43377b175069e55718f7ca15258c0a8e3c
路人甲 <717785320@qq.com>
2021-01-25 22:27:35 +0800

    update xhsjs.js.

--- xhsjs.js
+++ xhsjs.js
@@ -23,6 +23,7 @@ try {
     if (typeof $request != "undefined") {
         console.log("🍇小火山极速获取headers脚本开始!");
         console.log("body:" + $response.body);
+        console.log("headers:" + $request.headers);
         var headers = $request.headers;
         var headlist = $tool.getkeyval("xhsheadlist");
         $tool.setkeyval($request.url, "xhsurl");
