6b83b2295c1b9c63a87902d7106fa2ac76e4e2ad
路人甲 <717785320@qq.com>
2021-01-25 21:51:24 +0800

    update xhsjs.js.

--- xhsjs.js
+++ xhsjs.js
@@ -120,7 +120,7 @@ try {
 
             })
         }
-        else if (!headlist || headlist.length == 0) {
+        if (!headlist || headlist.length == 0) {
             $tool.notify("请先刷小火山视频获取headers", "多多益善", "");
             $done()
         }
