d3fa3c0565f1801c751ca25e139554fc3bf5a515
passerby-b <717785320@qq.com>
2021-02-07 13:16:12 +0800

    add

---
+++ scriptable/京东水果进度.js
@@ -0,0 +1,109 @@
+//京东水果成熟进度
+//var token = args.widgetParameter;
+
+var cookies = [
+  {
+    "name": "油腻大叔",
+    "cookie": "mba_muid=160792581193222731831; shshshfpb=sUfnzyxQ98ztRK2dmXcTTxA%3D%3D; shshshfpa=f9bdc6ab-9436-f3e2-5bc8-f4a42418e14e-1603764011; whwswswws=; cid=9; retina=1; webp=1; visitkey=65051204974215283; autoOpenApp_downCloseDate_auto=1608082945067_21600000; sc_width=375; __wga=1608083006044.1608082950691.1608082950691.1608082950691.7.1; __jdv=122270672%7Cdirect%7C-%7Cnone%7C-%7C1610500486108; __jdc=122270672; shshshfp=0a073de4222fecabf838f7f01964c40a; mobilev=html5; __jda=122270672.16080829218592127962767.1608082921.1610500486.1610526015.3; shshshsID=40a63e7bcb662e9e5cdab1b07cdefe46_1_1610526019733; 3AB9D23F7A4B3C9B=QKOGOBOT6DMO7YKFCJ3RRDORT5LAEH4HL7TAATQDT6TFICCK3FOUB5QWXXRFQXUOBJVUAXTGR6AP6RW3GQW6OT5EWA; jcap_dvzw_fp=SyP0g-mKRDl7dOqq0johCuFUZ9KqiEfjb-tIjO4PdKbBiNTh8E81nJ3iuzZ2fViECXnyRQ==; TrackerID=GL4Ka9lz6bHE4oy0kZdeJ6kxhEA635DZnsarLYOeIJW-UENkS18WiRlqmVHT4jTNDU7WTFZ8xOqYsHAoZbnGfg; pt_key=AAJf_q1ZADAwyTge9l0nKXg3I7_hAYr9EbYFW9ilIho9HtpbzZWm4ByZbbZgeagO40d-kCCaQAY; pt_pin=717785320_m; pt_token=gcqkmv7n; pwdt_id=717785320_m; sfstoken=tk01m976b1b0fa8sMisxeDF4M1JWlAiV3ZebynQ/3JAt2yQXzmt30K2x5f7/m4Kgw7UHXAFWsAk7JAAVLsrTgHM1+Gj7; __jdb=122270672.4.16080829218592127962767|3.1610526015; mba_sid=16105260153482488523324732827.4; __jd_ref_cls=JingDou_NewHome_SignContinue"
+  },
+  {
+    "name": "猫  女  神",
+    "cookie": "pt_key=AAJgD8z2ADD7z0UH5-lrWoUECZ8wtb6pebyc7uztZRwj_6Klfr32Z1tFDh6xwr47O4tdSc1VVyA; pt_pin=jd_4f59469c2f1e0;"
+  },
+  {
+    "name": "幸  运  草",
+    "cookie": "__jd_ref_cls=JDReact_StartReactModule; __jda=122270672.16108879889071713705179.1610887988.1610887988.1610887988.1; __jdb=122270672.6.16108879889071713705179|1.1610887988; __jdc=122270672; mba_muid=16108879889071713705179; mba_sid=16108879889098298889967872644.6; mobilev=html5; 3AB9D23F7A4B3C9B=RIMQM7NQHDLTNDT3J3NUGH62ZFZL6WTXPJQKLIIDVERUECSRF5TGCGGTXQULTV45Q4QMVEQ74TNSBJZXLCSWZ6QHTQ; shshshfpb=oGfRmBcQvnclPSponUQK4Xg==; shshshfp=efe9c5379900b0c18add9f9dc77adfa3; shshshfpa=ed606cba-d34c-16c3-973e-cae8e6c141e9-1610888008; shshshsID=6ff4d4be62b480990e32a0b0bd5d37dd_2_1610888106777; TrackerID=EZZ6mYyVcfoanzncNwhKI1GIuOH1G58z7Sxgv-mzg3DeWq77uCIvER8WAwqYBHCQGx45-I8xY-ii5d-wvmQhbxqw9LXdxyWDpkQjq10Q1TeOlUiCTgRiX3ipUIC8I4aJkr90bwTLB_8wmWtCX7XUHw; pt_key=AAJgBDOAADCnW345Llw6biid86F_ZLzealhhtz5ZKs3FHI86yDn-APXc6YL8UeZC2-5O9-w6WZc; pt_pin=jd_HSqaBIvRzvZJ; pt_token=x350jji0; pwdt_id=jd_HSqaBIvRzvZJ; sfstoken=tk01md2a71cc9a8sMSsxeDIrMXhjMiNcJiCjjbCqLATQKFtRsiFAhQOHbj7rbf3w5++4pPqdxzS9/rw0e+VATuylnb+U; whwswswws=; jcap_dvzw_fp=qcgRLoGTG_uc-uqPaUJpcvg86zfhCCfNK2YSQ1TesfFWaSx0FYaRSJ_qXFYah7XZTIo2tg==; __jdv=122270672|direct|-|none|-|1610887988908"
+  },
+  {
+    "name": "留　　言",
+    "cookie": "__jda=122270672.1610887634447244411245.1610887634.1610887634.1610887634.1; __jdb=122270672.6.1610887634447244411245|1.1610887634; __jdc=122270672; mba_muid=1610887634447244411245; mba_sid=1610887634454757059519345373.6; __jd_ref_cls=MJingDouHome_GoToJigsaw; TrackerID=LCVfJdeKjzBwB8s3VS6lPXznrpKZyBS6r3Ts0elewoCG4C2dakh3HGa1siUUJJgCgh1bX9aJDB4pYQW1k4BKWQFar3VwgWqpLq0S67OExmBWoOCxVo2I9EPxRSpt6hoDx_DRKEA8z3s8w0YghaLJfQ; pt_key=AAJgBDH9ADDj0iwFJkmj3Pu7zuedFHPk9vGSEKUPYMAG6vY6sfhLkFwJTPKhj7ipqC0zQzXxq_A; pt_pin=jd_75a96bdc9a5dd; pt_token=llxtki0l; pwdt_id=jd_75a96bdc9a5dd; sfstoken=tk01mca321ccaa8sMXgyKzF6SVFnUIrIdHGriEjwyO9r81TAoOYhkC2KUSHYNjvIbKDqZiWxxOFhEeJcxYCeizp8Y+4I; whwswswws=; shshshfpa=7ce45398-65fe-2fe2-198b-71e170fe409c-1602660623; shshshfpb=oaoW0deLOyWEaKcU9AOdwQw==; jcap_dvzw_fp=_ZyqQ2PcQ80i1iXaJsiiCqitgBZ3v-sPlQqzjvSczhyDyKWt4yqvTfqjM6-M3SV5NWOEXw==; 3AB9D23F7A4B3C9B=52BE3OMJVZUPTWEHPVZGD6MFGAP7XRAKZHH4FJSNTZH5TLNWNBZ3UFMEW2Y4ZH2IDXRNCE7IDDDSLXMWG4BPPAGATM; shshshfp=efe9c5379900b0c18add9f9dc77adfa3; shshshsID=bebe5f2f4fbfec7d396fbfac4f69836f_1_1610887651956; mobilev=html5; __jdv=122270672|direct|-|none|-|1610887634449; unpl=V2_ZzNtbUBQFxcmAUFUcx8IDGJWFQ5KBEcQcw9BV3MbX1AwBhRaclRCFnQURldnGlQUZAAZWUFcRhRFCENkexhdBGMHE19FXnMlRQtGZHsYbAVgAhpeRlVHEnQOTlx9GlQCZAMbXEVncxJ1AXZkrpLB0cCuxMr8g/qNo53JZHscVQJiBxpaQVFzFEUKR1V4HlsGYAYQbQk5Qlh1D0dceB1eAWACFFVKUUAdcgtGXXoebARXAA==; __jdu=160502916614344961047; pinId=vqkZfdvkI6yiCaAyEiYdTQ"
+  },
+  {
+    "name": "某人某天",
+    "cookie": ""
+  },
+
+];
+
+
+let widget = await createWidget()
+if (!config.runsInWidget) {
+  await widget.presentLarge()
+}
+Script.setWidget(widget)
+Script.complete()
+async function createWidget() {
+  let title = "京东水果成熟进度"
+  let w = new ListWidget()
+  bg = new LinearGradient()
+  bg.locations = [0, 1]
+  bg.colors = [
+    new Color("#6fa8dc"),
+    new Color("#a4c2f4")
+  ]
+
+  w.backgroundGradient = bg
+  w.addSpacer(8)
+
+  // 显示图标和标题
+  let titleStack = w.addStack()
+  titleStack.addSpacer(4)
+  let titleElement = titleStack.addText(title)
+  titleElement.textColor = Color.white()
+  titleElement.font = Font.mediumSystemFont(15)
+  w.addSpacer(8)
+
+  for (var i = 0; i < cookies.length; i++) {
+    let data = await getData(cookies[i].cookie);
+    let msgstr = "";
+    if (!!data.msg && data.msg == "not login") {
+      msgstr = cookies[i].name + ": cookie失效";
+    }
+    else if (data.farmUserPro.treeState == 2 || data.farmUserPro.treeState == 3) {
+      msgstr = cookies[i].name + ": 可领取";
+    }
+    else if (data.farmUserPro.treeState == 0) {
+      msgstr = cookies[i].name + ": 请种植水果";
+    }
+    else {
+      var str = data.farmUserPro.name + "，";
+      var str2 = "进度" + ((data.farmUserPro.treeEnergy / data.farmUserPro.treeTotalEnergy) * 100).toFixed(2) + "%，";
+      var str3 = "还剩" + (data.farmUserPro.treeTotalEnergy - data.farmUserPro.treeEnergy) / 10 + "次";
+      msgstr = cookies[i].name + ": " + str + str2 + str3;
+    }
+    if (!!data.farmUserPro && !!data.farmUserPro.shareCode) {
+      console.log(cookies[i].name + "分享码: " + data.farmUserPro.shareCode);
+    }
+
+    let date1 = w.addText(msgstr)
+    date1.font = Font.semiboldSystemFont(12)
+    date1.textColor = Color.white()
+    w.addSpacer(5)
+
+  }
+
+  // 更新时间
+  let gx = '更新' + new Date().getFullYear() + "-" + new Date().getMonth() + "-" + new Date().getDay() + " " + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds();
+  let body = w.addText(gx)
+  body.font = Font.mediumRoundedSystemFont(9)
+  body.textColor = Color.white()
+  w.addSpacer(15)
+  return w
+}
+async function getData(cookie) {
+  var url = 'https://api.m.jd.com/client.action?functionId=initForFarm';
+
+  var req = new Request(url)
+
+  req.headers = {
+    'User-Agent': 'jdapp;iPhone;9.2.2;14.2;%E4%BA%AC%E4%B8%9C/9.2.2 CFNetwork/1206 Darwin/20.1.0',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    "cookie": cookie
+  };
+  req.method = 'POST';
+  req.body = "body=version:4&appid=wh5&clientVersion=9.1.0";
+  //console.log(req);
+  var data = await req.loadJSON();
+  //console.log(data);
+  return data
+}
