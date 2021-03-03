77fa296d7d02727b385ffc0098e08ba7c9d6f1c3
passerby-b <717785320@qq.com>
2021-03-03 13:27:43 +0800

    add

---
+++ test/xxxxxxxx.js
@@ -0,0 +1,7 @@
+
+/**
+ * @fileoverview Template to compose HTTP reqeuest.
+ * 
+ */
+$request.headers['User-Agent'] = "Mi Note 3(Linux; Android 10) DiDiApp(OneTravel/6.1.9) Weex/0.24.0 ExternalUA 1242x2688";
+$done({ headers: $request.headers });
