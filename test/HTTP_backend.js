81558866b5b5d37ed81269ff6e9874cc704de0a2
passerby-b <717785320@qq.com>
2021-03-09 21:15:34 +0800

    update test/HTTP_backend.js.

--- test/HTTP_backend.js
+++ test/HTTP_backend.js
@@ -7,7 +7,7 @@ var html = '<!DOCTYPE html><html lang="en"xmlns="http://www.w3.org/1999/xhtml"><
     //$done(d);
 //});
 try{
- var str= $prefs.valueForKey('dyheadlist');
+ var str= $prefs.valueForKey('thisurl');
  $done(str);
 }
 catch(e){
