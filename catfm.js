19ef191c28e3d308f8d07d19c9d3b6870e4657ab
passerby-b <717785320@qq.com>
2021-03-30 11:00:22 +0800

    update catfm.js.

--- catfm.js
+++ catfm.js
@@ -1,15 +1,9 @@
 
 var res = JSON.parse($response.body);
 
-res.info.drama.pay_type = 0;
-res.info.drama.type = 0;
-res.info.drama.need_pay = 0;
+res.info.price=0;
+res.info.need_pay = 0;
 
-for (let index = 0; index < res.info.episodes.episode.length; index++) {
-    res.info.episodes.episode[index].pay_type = 0;
-    res.info.episodes.episode[index].type = 0;
-    res.info.episodes.episode[index].need_pay = 0;
-}
 console.log(JSON.stringify(res));
 $done({ body: JSON.stringify(res) })
 
