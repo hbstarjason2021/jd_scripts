ad579d3c907ce393c32a0e068c457bd52deae47e
passerby-b <717785320@qq.com>
2021-03-30 11:30:36 +0800

    删除文件 catfm copy.js

--- catfm copy.js
+++
@@ -1,9 +0,0 @@
-var res = JSON.parse($response.body);
-
-
-//res.info.pay_type = 0;
-res.info.price = 0;
-res.info.need_pay = 0;
-
-console.log(JSON.stringify(res));
-$done({ body: JSON.stringify(res) })
