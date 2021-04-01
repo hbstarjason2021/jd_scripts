2a7a515c8c9c69ae0619c07d5ce80d4df4e08383
passerby-b <717785320@qq.com>
2021-04-01 17:50:10 +0800

    update test/HTTP_backend.js.

--- test/HTTP_backend.js
+++ test/HTTP_backend.js
@@ -8,7 +8,8 @@ var html = '<!DOCTYPE html><html lang="en"xmlns="http://www.w3.org/1999/xhtml"><
 //});
 try{
   //var str= $prefs.valueForKey('dyheadlist');
-  var str= $prefs.valueForKey('zqgetbody_body');
+  //var str= $prefs.valueForKey('zqgetbody_body');
+  var str= $prefs.valueForKey('中青看点阅读');
   $done(str);
 }
 catch(e){
