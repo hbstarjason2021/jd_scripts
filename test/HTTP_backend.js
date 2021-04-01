6c1dfa06db7554c8bf7db6d4952d4175267d0d53
passerby-b <717785320@qq.com>
2021-04-01 11:32:50 +0800

    update test/HTTP_backend.js.

--- test/HTTP_backend.js
+++ test/HTTP_backend.js
@@ -7,7 +7,8 @@ var html = '<!DOCTYPE html><html lang="en"xmlns="http://www.w3.org/1999/xhtml"><
     //$done(d);
 //});
 try{
-  var str= $prefs.valueForKey('dyheadlist');
+  //var str= $prefs.valueForKey('dyheadlist');
+  var str= $prefs.valueForKey('zqgetbody_body');
   $done(str);
 }
 catch(e){
