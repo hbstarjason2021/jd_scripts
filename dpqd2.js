43fd5d78dcc233e05b072be7f1bb73623655d528
ljqailym <63405597+ljqailym@users.noreply.github.com>
2021-04-20 21:25:58 +0800

    Update dpqd2.js

--- dpqd2.js
+++ dpqd2.js
@@ -2,14 +2,14 @@
 ============Quantumultx===============
 [task_local]
 #店铺签到
-0 0 0 * * * https://raw.githubusercontent.com/ljqailym/JD/main/dpqd2.js, tag=店铺签到, enabled=true
+0 0 * * * https://raw.githubusercontent.com/ljqailym/JD/main/dpqd2.js, tag=店铺签到, enabled=true
 ===========Loon============
 [Script]
-cron "0 0 0 * * *" script-path=https://raw.githubusercontent.com/ljqailym/JD/main/dpqd2.js,tag=店铺签到
+cron "0 0 * * *" script-path=https://raw.githubusercontent.com/ljqailym/JD/main/dpqd2.js,tag=店铺签到
 ============Surge=============
-店铺签到 = type=cron,cronexp="0 0 0 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/ljqailym/JD/main/dpqd2.js
+店铺签到 = type=cron,cronexp="0 0 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/ljqailym/JD/main/dpqd2.js
 ===========小火箭========
-店铺签到 = type=cron,script-path=https://raw.githubusercontent.com/ljqailym/JD/main/dpqd2.js, cronexpr="0 0 0 * * *", timeout=3600, enable=true
+店铺签到 = type=cron,script-path=https://raw.githubusercontent.com/ljqailym/JD/main/dpqd2.js, cronexpr="0 0 * * *", timeout=3600, enable=true
 */
 
 const $ = new Env('店铺签到');
