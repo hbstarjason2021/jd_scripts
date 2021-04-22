a3ab274dfd67609628dc1496acc5bb7d77a183ab
ljqailym <63405597+ljqailym@users.noreply.github.com>
2021-04-22 13:15:38 +0800

    Update redman_rain.js

--- redman_rain.js
+++ redman_rain.js
@@ -14,7 +14,7 @@ let ids = {
   '0': 'RRA3YwTApQMXk5yvcjLsADfbYkNAgv6',
   '12': '',
   '13': 'RRA3CMggYFzqDma3Pz9DbPbFPs8unYk',
-  '14': '',
+  '14': 'RRA85AvVYJBvkmwpDhn4Y9cMRXV9mH',
   '15': 'RRAAYPHu6s5pWzYMqmfNygvvgJTKdq',
   '16': 'RRA2u4bEZ2zLYr9PKfwqfXngbVCmqNE',
   '17': 'RRA2wKRg7gL9Ns1zqtun1JwkBTZ2yVS',
