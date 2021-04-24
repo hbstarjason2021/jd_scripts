8c8ba3737d927256075dfc6a2c2efc6490fc025e
ljqailym <63405597+ljqailym@users.noreply.github.com>
2021-04-24 18:33:41 +0800

    Update dpqd3.js

--- dpqd3.js
+++ dpqd3.js
@@ -26,7 +26,7 @@ let vender=''
 let num=0
 let shopname=''
 const token=[
- '0FE365EE2D7E143502CFA1A73606ADB8',//3天100豆4.18
+ '3D68AA11D3DA9A88525795EC0F68F815',//3天100豆4.18
   
 ]
 //IOS等用户直接用NobyDa的jd cookie
