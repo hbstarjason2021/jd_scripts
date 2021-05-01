938589969ed48fcd6be429fe1f4725c104b549e1
ljqailym <63405597+ljqailym@users.noreply.github.com>
2021-05-01 21:37:38 +0800

    Update dpqd2.js

--- dpqd2.js
+++ dpqd2.js
@@ -26,7 +26,7 @@ let vender=''
 let num=0
 let shopname=''
 const token=[
- '99AA606C96516916CF080259E76E0DD9',//3天100豆4.18
+ 'CDD63FB6888FAC7DA8AAF936AFE51094',//3天100豆4.18
   
 ]
 //IOS等用户直接用NobyDa的jd cookie
